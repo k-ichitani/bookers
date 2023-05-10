cf018e1c39fbdf6e4ebffcca1c3ee90f2519c90d
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-05-10 09:37:21 +0000

    20230510 commit4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,17 @@
 class BooksController < ApplicationController
   def new
-    @Book = Book.new
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.news
   end
 
   def show
